0cebf427c8968cb7f10d625924c41760a82d1c38
pimyn-girgis <bemenboshra2001@gmail.com>
2023-05-05 18:31:05 +0300

    Revert "removed some files"
    This reverts commit 6116c5f9a96ff392bdbbced3e6ca857440333f4e.

---
+++ CMakeFiles/RISC-V_Simulator.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RISC-V_Simulator.dir/src/genUtils.o"
+  "CMakeFiles/RISC-V_Simulator.dir/src/genUtils.o.d"
+  "CMakeFiles/RISC-V_Simulator.dir/src/main.o"
+  "CMakeFiles/RISC-V_Simulator.dir/src/main.o.d"
+  "CMakeFiles/RISC-V_Simulator.dir/src/memory.o"
+  "CMakeFiles/RISC-V_Simulator.dir/src/memory.o.d"
+  "CMakeFiles/RISC-V_Simulator.dir/src/parser.o"
+  "CMakeFiles/RISC-V_Simulator.dir/src/parser.o.d"
+  "RISC-V_Simulator"
+  "RISC-V_Simulator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RISC-V_Simulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
